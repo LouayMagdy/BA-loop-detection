4b6cd200669e1385abe48d06449183519a41a4ee
LouayMagdy <magdylouay@gmail.com>
2023-07-19 18:21:51 +0300

    initial commit

---
+++ loop-detection-backend/src/main/java/com/example/loopdetectionbackend/LoopDetectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.loopdetectionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopDetectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoopDetectionBackendApplication.class, args);
+    }
+
+}
---
+++ loop-detection-backend/src/test/java/com/example/loopdetectionbackend/LoopDetectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loopdetectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopDetectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
